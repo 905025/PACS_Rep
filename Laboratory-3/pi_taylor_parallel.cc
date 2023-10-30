ac53a4c002318413387a5e397f53acc12bcba4f2
JorgeCarballoGitHub <jorge.carballovicente12@gmail.com>
2023-10-30 23:23:06 +0100

    u

--- Laboratory-3/pi_taylor_parallel.cc
+++ Laboratory-3/pi_taylor_parallel.cc
@@ -1,30 +1,43 @@
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
 #include <limits>
-#include <numeric>
-#include <string>
-#include <thread>
-#include <utility>
 #include <vector>
+#include <thread>
+#include <mutex>
 
 using my_float = long double;
 
-
-void pi_taylor_chunk(std::vector<my_float> &output, size_t start, size_t end, size_t thread_id, size_t num_threads) {
+void pi_taylor_chunk(std::vector<my_float> &output, size_t start, size_t end) {
     my_float chunk_result = 0.0;
+    int sign = (start % 2 == 0) ? 1 : -1;  // Random sign in the init
     for (size_t n = start; n < end; n++) {
-        if (n % num_threads == thread_id) {
-            chunk_result += 1.0 / static_cast<my_float>(2 * n + 1);
-        }
+        chunk_result += sign / static_cast<my_float>(2 * n + 1);
+        sign = -sign;  // Alternate sign 
     }
-    output[thread_id] = chunk_result;
+    output[0] += chunk_result;
 }
 
+my_float pi_taylor(size_t steps, size_t num_threads) {
+    std::vector<my_float> thread_results(num_threads, 0.0);
+    std::vector<std::thread> worker_threads(num_threads);
+    size_t chunk_size = steps / num_threads;
 
+    for (size_t i = 0; i < num_threads; i++) {
+        size_t start = i * chunk_size;
+        size_t end = (i == num_threads - 1) ? steps : (i + 1) * chunk_size;
+        worker_threads[i] = std::thread(pi_taylor_chunk, std::ref(thread_results), start, end);
+    }
 
-std::pair<size_t, size_t>
-usage(int argc, const char *argv[]) {
-    // read the number of steps from the command line
+    for (size_t i = 0; i < num_threads; i++) {
+        worker_threads[i].join();
+    }
+
+    my_float pi = thread_results[0];
+
+    return 4.0 * pi;
+}
+
+std::pair<size_t, size_t> usage(int argc, const char *argv[]) {
     if (argc != 3) {
         std::cerr << "Invalid syntax: pi_taylor <steps> <threads>" << std::endl;
         exit(1);
@@ -33,40 +46,25 @@ usage(int argc, const char *argv[]) {
     size_t steps = std::stoll(argv[1]);
     size_t threads = std::stoll(argv[2]);
 
-    if (steps < threads ){
+    if (steps <= threads) {
         std::cerr << "The number of steps should be larger than the number of threads" << std::endl;
         exit(1);
-
     }
+
     return std::make_pair(steps, threads);
 }
 
 int main(int argc, const char *argv[]) {
-
-
     auto ret_pair = usage(argc, argv);
     auto steps = ret_pair.first;
     auto threads = ret_pair.second;
 
-    std::vector<my_float> thread_results(threads);
-    std::vector<std::thread> worker_threads(threads);
+    auto pi = pi_taylor(steps, threads);
 
-    for (size_t i = 0; i < threads; i++) {
-        worker_threads[i] = std::thread(pi_taylor_chunk, std::ref(thread_results), i, steps, i, threads);
-    }
+    std::cout << "For " << steps << " steps and " << threads << " threads, pi value: "
+              << std::setprecision(std::numeric_limits<my_float>::digits10 + 1)
+              << pi << std::endl;
 
-    for (size_t i = 0; i < threads; i++) {
-        worker_threads[i].join();
-    }
-
-    my_float pi = std::accumulate(thread_results.begin(), thread_results.end(), 0.0);
-    pi *= 4.0;
-
-
-    std::cout << "For " << steps << ", pi value: "
-        << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
-        << pi << std::endl;
-
-        return 0 ;
+    return 0;
 }
 
